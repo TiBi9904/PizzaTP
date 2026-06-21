0cc7c2a30268ab03bdd8d36a795d5fd0c079bea7
Bùi Hải Trọng <buihaitrong.dev@gmail.com>
2026-06-21 23:24:02 +0700

    Update sendEmail.js

--- Backend/config/sendEmail.js
+++ Backend/config/sendEmail.js
@@ -8,7 +8,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async ({ sendTo, subject, html }) => {
   try {
     const response = await resend.emails.send({
-      from: process.env.EMAIL_FROM,
+      from: `PizzaTP <${process.env.EMAIL_FROM}>`,
       to: sendTo,
       subject,
       html,
